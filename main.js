d2bb1e52ee0cf02f4a0374de79ee373ae1dd5994
Selwyn <talk@selwyn.cc>
2020-04-22 11:23:48 +0200

    Use branch name when available

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ core.debug(`GitHub Event Name: ${process.env.GITHUB_EVENT_NAME}`);
 const eventData = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH));
 
 const siteName = core.getInput('site-name') || eventData.repository.name;
+
+const branchPrefix = eventData.ref && `${eventData.ref.split('/').pop()}--` || '';
+
 const basicAuthUsername = core.getInput('basic-auth-username');
 const basicAuthPassword = core.getInput('basic-auth-password');
 const basicAuth = basicAuthUsername && basicAuthPassword
@@ -20,5 +23,5 @@ core.setOutput(
   'url',
   eventData.number
     ? `https://${basicAuth}deploy-preview-${eventData.number}--${siteName}.netlify.app/`
-    : `https://${basicAuth}${siteName}.netlify.app/`
+    : `https://${basicAuth}${branchPrefix}${siteName}.netlify.app/`
 );
